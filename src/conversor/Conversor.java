17dd443537bb563b98e25af21b93e1abe6db2398
Héctor <hectoruu@engineer.com>
2023-07-06 01:53:07 -0600

    Inicio del proyecto

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,12 @@
+package conversor;
+
+import javax.swing.JOptionPane;
+
+public class Conversor {
+
+	public static void main(String ... H) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Inicio del proyecto");
+	}
+
+}
