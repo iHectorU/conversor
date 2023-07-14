b3fe18294ccee2cd016af10bc4d31c79a0ed2860
Héctor <hectoruu@engineer.com>
2023-07-14 12:23:10 -0600

    Creación de funcionalidad y ventanas faltantes con validación de datos.

--- src/com/one/conversor/latam/Conversor.java
+++ src/com/one/conversor/latam/Conversor.java
@@ -2,8 +2,48 @@ package com.one.conversor.latam;
 
 public class Conversor {
 	
-	public double pesoDolar(double valor) {
-		return valor * 0.059;
+	public double pesoDolar(String valor) {
+		return Double.parseDouble(valor) * 0.059;
+	}
+	
+	public double pesoEuro(String valor) {
+		return Double.parseDouble(valor) * 0.053;
+	}
+	
+	public double pesoLibra(String valor) {
+		return Double.parseDouble(valor) * 0.045;
+	}
+	
+	public double pesoYen(String valor) {
+		return Double.parseDouble(valor) * 8.27;
+	}
+	
+	public double pesoWonCoreano(String valor) {
+		return Double.parseDouble(valor) * 75.71;
+	}
+	
+	public double dolarPeso(String valor) {
+		return Double.parseDouble(valor) * 16.78;
+	}
+	
+	public double euroPeso(String valor) {
+		return Double.parseDouble(valor) * 18.85;
+	}
+	
+	public double libraPeso(String valor) {
+		return Double.parseDouble(valor) * 21.99;
+	}
+	
+	public double yenPeso(String valor) {
+		return Double.parseDouble(valor) * 0.12;
+	}
+	
+	public double wonCoreanoPeso(String valor) {
+		return Double.parseDouble(valor) * 0.013;
+	}
+	
+	public boolean validarNumero(String valor) {
+		return valor.matches("[0-9]*");
 	}
 
 }
--- src/com/one/conversor/latam/ConversorApp.java
+++ src/com/one/conversor/latam/ConversorApp.java
@@ -16,60 +16,66 @@ public class ConversorApp {
 		do {
 			String divisa = JOptionPane.showInputDialog(null, "Seleccione una opción", "Conversor", JOptionPane.CLOSED_OPTION, null, new Object[] {"Conversor de moneda", "Conversor de Temperatura", "Conversor de medidas"}, "Conversor de moneda").toString();
 			if (divisa.equals("Conversor de moneda")) {
-				double valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor"));
-				
-				tipoCambio = JOptionPane.showInputDialog(null, "Elija la conversión que desea", "Tipo de cambio", JOptionPane.CLOSED_OPTION, null, new Object[] {"De Pesos a Dólar", "De Pesos a Euro", "De Pesos a Libra", "De Pesos a Yen", "De Pesos a Won Coreano", "De Dólar a Pesos", "De Euro a Pesos", "De Libra a Pesos", "De Yen a Pesos", "De Won Coreano a Pesos"}, "De Pesos a Dólar").toString();
-				switch(tipoCambio) {
-				case "De Pesos a Dólar" :
-					resultado = conversor.pesoDolar(valor);
-					JOptionPane.showMessageDialog(null, "US$" + resultado);
-					break;
-					
-				case "De Pesos a Euro" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "€" + resultado);
-					break;
-					
-				case "De Pesos a Libra" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "£" + resultado);
-					break;
-					
-				case "De Pesos a Yen" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "¥" + resultado);
-					break;
-					
-				case "De Pesos a Won Coreano" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "₩" + resultado);
-					break;
-					
-				case "De Dólar a Peso" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "$" + resultado);
-					break;
-					
-				case "De Euro a Peso" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "€" + resultado);
-					break;
-					
-				case "De Libra a Peso" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "€" + resultado);
-					break;
-					
-				case "De Yen a Peso" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "€" + resultado);
-					break;
-					
-				case "De Won Coreano a Peso" :	
-					resultado = 0.0;
-					JOptionPane.showMessageDialog(null, "€" + resultado);
-					break;
+				String valor = JOptionPane.showInputDialog("Ingrese el valor");
+				if (!conversor.validarNumero(valor)) {
+					JOptionPane.showMessageDialog(null, "Ingrese solo números");
+				} else {
+					tipoCambio = JOptionPane.showInputDialog(null, "Elija la conversión que desea", "Tipo de cambio", JOptionPane.CLOSED_OPTION, null, new Object[] {"De Pesos a Dólar", "De Pesos a Euro", "De Pesos a Libra", "De Pesos a Yen", "De Pesos a Won Coreano", "De Dólar a Pesos", "De Euro a Pesos", "De Libra a Pesos", "De Yen a Pesos", "De Won Coreano a Pesos"}, "De Pesos a Dólar").toString();
+					switch(tipoCambio) {
+					case "De Pesos a Dólar" :
+						resultado = conversor.pesoDolar(valor);
+						JOptionPane.showMessageDialog(null, "US$" + resultado);
+						break;
+						
+					case "De Pesos a Euro" :	
+						resultado = conversor.pesoEuro(valor);
+						JOptionPane.showMessageDialog(null, "€" + resultado);
+						break;
+						
+					case "De Pesos a Libra" :	
+						resultado = conversor.pesoLibra(valor);
+						JOptionPane.showMessageDialog(null, "£" + resultado);
+						break;
+						
+					case "De Pesos a Yen" :	
+						resultado = conversor.pesoYen(valor);
+						JOptionPane.showMessageDialog(null, "¥" + resultado);
+						break;
+						
+					case "De Pesos a Won Coreano" :	
+						resultado = conversor.pesoWonCoreano(valor);
+						JOptionPane.showMessageDialog(null, "₩" + resultado);
+						break;
+						
+					case "De Dólar a Peso" :	
+						resultado = conversor.dolarPeso(valor);
+						JOptionPane.showMessageDialog(null, "$" + resultado);
+						break;
+						
+					case "De Euro a Peso" :	
+						resultado = conversor.euroPeso(valor);
+						JOptionPane.showMessageDialog(null, "€" + resultado);
+						break;
+						
+					case "De Libra a Peso" :	
+						resultado = conversor.libraPeso(valor);
+						JOptionPane.showMessageDialog(null, "€" + resultado);
+						break;
+						
+					case "De Yen a Peso" :	
+						resultado = conversor.yenPeso(valor);
+						JOptionPane.showMessageDialog(null, "€" + resultado);
+						break;
+						
+					case "De Won Coreano a Peso" :	
+						resultado = conversor.wonCoreanoPeso(valor);
+						JOptionPane.showMessageDialog(null, "€" + resultado);
+						break;
+					}
 				}
+				
+				
+				
 				
 				
 			} else if (divisa.equals("Conversor de Temperatura")) {
